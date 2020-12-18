4a224b2be669d541a9b9691d14ced30dda0da8c5
Lucas Pagac <lucaspagac0@gmail.com>
2020-12-17 17:06:10 -0800

    tests for user signup and authenticate written

--- test_user_model.py
+++ test_user_model.py
@@ -20,7 +20,7 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 # Now we can import app
 
 from app import app
-
+from sqlalchemy.exc import IntegrityError
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
@@ -52,6 +52,14 @@ class UserModelTestCase(TestCase):
     def test_user_model(self):
         """Does basic model work?"""
 
+        # User should have no messages & no followers
+        self.assertEqual(len(self.u.messages), 0)
+        self.assertEqual(len(self.u.followers), 0)
+        self.assertIn("test@test.com", repr(self.u))
+        
+    def test_user_follow_methods(self):
+        """ Do is_following and is_followed_by work? """   
+
         u2 = User(
             email="test2@test.com",
             username="testuser2",
@@ -59,13 +67,57 @@ class UserModelTestCase(TestCase):
         )
         db.session.add(u2)
         db.session.flush()
-        # User should have no messages & no followers
-        self.assertEqual(len(self.u.messages), 0)
-        self.assertEqual(len(self.u.followers), 0)
-        self.assertIn("test@test.com", repr(self.u))
 
         self.u.followers.append(u2)
         self.u.following.append(u2)
         db.session.commit()
 
-        self.assertEqual(self.u.is_following(u2))
+        self.assertEqual(self.u.is_following(u2), True)
+        self.assertEqual(self.u.is_followed_by(u2), True)
+        self.u.following.remove(u2)
+        self.u.followers.remove(u2)
+        db.session.commit()
+        self.assertEqual(self.u.is_followed_by(u2), False)
+        self.assertEqual(self.u.is_followed_by(u2), False)
+
+    def test_user_signup(self):
+        """ Does User.signup method work? """ 
+
+        user_signup_test = User.signup(
+            "signup_test",
+            "signup@test.com",
+            "HASHED_PASSWORD",
+            None
+        )
+        db.session.commit()
+        self.assertIsInstance(user_signup_test, User)
+        
+        User.signup(
+            "signup_test",
+            "signup_invalid@test.com",
+            "HASHED_PASSWORD",
+            None
+        )
+        try:
+            db.session.commit()
+        except IntegrityError:
+            error = True
+            
+        self.assertTrue(error)
+
+    def test_user_authenticate(self):
+        """ Does User.authenticate work? """
+
+        user_signup = User.signup(
+            "signup_test",
+            "signup@test.com",
+            "HASHED_PASSWORD",
+            None
+        )
+        db.session.add(user_signup)
+        db.session.commit()
+        user_authenticate = User.authenticate("signup_test", "HASHED_PASSWORD")
+        self.assertIs(user_authenticate, user_signup)
+
+        self.assertFalse(User.authenticate('signup_test', "Invalid password"))
+        self.assertFalse(User.authenticate('Invalid_username', "HASHED_PASSWORD"))
